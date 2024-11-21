dcecdea836ae384d4d8a7da34116b707ec57815b
Yash25032003 <117072378+Yash25032003@users.noreply.github.com>
2024-11-21 18:03:15 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
       const parsedInput = JSON.parse(sanitizedInput);
 
       // Make API call
-      const res = await axios.post("http://localhost:5000/bfhl", parsedInput);
+      const res = await axios.post("https://solution1ofbj-backend.onrender.com/bfhl", parsedInput);
       setResponse(res.data);
     } catch (error) {
       console.log("Error:", error);
